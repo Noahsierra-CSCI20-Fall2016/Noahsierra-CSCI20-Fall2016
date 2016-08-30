3ec6c0631a4f7ae7ce6e8501f55417f67ada2778
Noah <nsierra001@student.butte.edu>
2016-08-30 18:43:01 +0000

    Lab2 folder updated for algorithm

---
+++ Lab2/lab2.cpp
@@ -0,0 +1,5 @@
+//  Choose a number from an option of two numbers. 
+//  If right number is chosen then display "you win" and give 10 points.
+//If not then display "higher or Lower" and subtract one point. 
+      //let them guess until they got it right. 
+        
