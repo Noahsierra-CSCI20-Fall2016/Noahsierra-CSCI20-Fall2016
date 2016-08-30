1520f4d9e5d963810885de938445a1a816340dd1
Noah <nsierra001@student.butte.edu>
2016-08-30 01:32:26 +0000

    Lab1 folder updated for ASCII art

---
+++ Lab1/lab1.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std; 
+
+int main () {
+    cout << "///_______///" <<endl;
+    cout << "///^     ^///"  << endl;
+    cout << "///   v   ///" << endl;
+    cout << " ///______///" << endl;  
+    cout << "///  ( ) ///" << endl;
+    
+
+    return 0; 
+} 
